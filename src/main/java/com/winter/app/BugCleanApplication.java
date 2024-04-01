f10f1b2f6f5ddd4bd2f03e45f8bb3a2b30b449f2
sang <xhdfud1@naver.com>
2024-04-01 12:33:19 +0900

    2024 04 01 기본

---
+++ src/main/java/com/winter/app/BugCleanApplication.java
@@ -0,0 +1,13 @@
+package com.winter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugCleanApplication.class, args);
+	}
+
+}
