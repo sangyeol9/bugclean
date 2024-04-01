9fd7a3d2e8f1764f1fcfea1669dbe261c66c0a3e
SeokHoon <shheo90oi@gmail.com>
2024-04-01 17:30:32 +0900

    노찌롱

--- src/main/java/com/winter/app/BugCleanApplication.java
+++ src/main/java/com/winter/app/BugCleanApplication.java
@@ -8,7 +8,7 @@ public class BugCleanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BugCleanApplication.class, args);
-		System.out.println("노지언 멍청이");
+		System.out.println("노지언 멍청이 말미잘");
 	}
 
 }
