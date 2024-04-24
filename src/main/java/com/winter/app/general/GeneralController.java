c68c017adb948b17d96b66f96bacb6a488046438
SeokHoon <sughoon1@gmail.com>
2024-04-24 16:02:47 +0900

    11

---
+++ src/main/java/com/winter/app/general/GeneralController.java
@@ -0,0 +1,11 @@
+package com.winter.app.general;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/general")
+public class GeneralController {
+}
