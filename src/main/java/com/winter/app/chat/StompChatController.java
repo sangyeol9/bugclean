6e368733d7c91c6caea61109dcf8f39b3995bc85
sang <xhdfud1@naver.com>
2024-04-30 20:34:16 +0900

    2024 04 30
    집가서 채팅내역 뿌려주기 하면됩니다.

--- src/main/java/com/winter/app/chat/StompChatController.java
+++ src/main/java/com/winter/app/chat/StompChatController.java
@@ -14,13 +14,12 @@ public class StompChatController {
 	
 	@MessageMapping(value = "/chat/enter")
 	public void enter(ChatMessage message) {
-		message.setMessage(message.getWriter() + "님이 채팅방에 입장하셨습니다.");
-		template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+		
 	}
 	
 	@MessageMapping(value = "/chat/message")
 	public void message(ChatMessage message) {
-		template.convertAndSend("/sub/chat/room/"+ message.getRoomId(), message);
+		template.convertAndSend("/sub/chat/room/"+ message.getRoom_num(), message);
 	}
 	
 	
